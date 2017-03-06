61488af2bd373f34c97d4326852063f7025843f1
Adam Scott <ascott1@gmail.com>
2017-03-05 22:08:29 -0500

    ajax POST and DOM content insertion

--- public/main.js
+++ public/main.js
@@ -1,1 +1,28 @@
-console.log('JS loaded')
+var form = document.getElementById('sign-up')
+var submit = document.getElementById('submit')
+var ul = document.getElementById('name-list')
+
+var submitRequest = function(e) {
+  e.preventDefault();
+
+  var data = {
+    'first': document.getElementById('first').value,
+    'last': document.getElementById('last').value
+  }
+
+  xhr = new XMLHttpRequest()
+  xhr.open('POST', '/names')
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.send(JSON.stringify(data))
+
+  appendNewItem(data)
+}
+
+var appendNewItem = function(name) {
+  let li = document.createElement('li')
+  li.appendChild(document.createTextNode(name.first + ' ' + name.last))
+  ul.appendChild(li)
+  form.reset()
+}
+
+submit.addEventListener('click', submitRequest, false )
