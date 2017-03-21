bd4454924824c3f1f798e22ddec9d81660663a3a
Adam Scott <ascott1@gmail.com>
2017-03-21 06:24:11 -0400

    add simple service worker

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+// cache version
+const cacheName = 'v1::static';
+
+self.addEventListener('install', e => {
+  // once the SW is installed, go ahead and fetch the resources
+  // to make this work offline
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+        'main.js',
+        'main.js',
+        'styles.css',
+        'marsbq_logo.png'
+      ]).then(() => self.skipWaiting());
+    })
+  );
+});
+
+// when the browser fetches a url, either response with
+// the cached object or go ahead and fetch the actual url
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    // ensure we check the *right* cache to match against
+    caches.open(cacheName).then(cache => {
+      return cache.match(event.request).then(res => {
+        return res || fetch(event.request)
+      });
+    })
+  );
+});
